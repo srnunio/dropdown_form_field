720701b8453a72bfd85845875dff630fd9c6837c
Edvaldo Martins <edvaldomartins.developer@gmail.com>
2024-09-30 14:58:24 +0100

    config onTap item

--- lib/dropdown_form_field.dart
+++ lib/dropdown_form_field.dart
@@ -17,24 +17,28 @@ class _Builder extends StatelessWidget {
 
   final Function(dynamic)? onChanged;
 
+  final Function()? onTap;
+
   final Widget hintTitle;
 
   final Color? iconDisabledColor;
   final Color? iconEnabledColor;
   final Color? dropdownColor;
 
-  _Builder(
-      {required this.state,
-        required this.items,
-        required this.decoration,
-        required this.hintTitle,
-        required this.selectedValue,
-        required this.builderItem,
-        required this.builderSelectedItem,
-        required this.iconDisabledColor,
-        required this.iconEnabledColor,
-        required this.dropdownColor,
-        required this.onChanged});
+  _Builder({
+    required this.state,
+    required this.items,
+    required this.decoration,
+    required this.hintTitle,
+    required this.selectedValue,
+    required this.builderItem,
+    required this.builderSelectedItem,
+    required this.iconDisabledColor,
+    required this.iconEnabledColor,
+    required this.dropdownColor,
+    required this.onChanged,
+    required this.onTap,
+  });
 
   _onChanged(dynamic newValue) {
     state.didChange(newValue);
@@ -45,11 +49,12 @@ class _Builder extends StatelessWidget {
   List<DropdownMenuItem> _items(BuildContext context) {
     return items
         .map((item) => DropdownMenuItem<dynamic>(
-      value: item,
-      child: Container(
-          alignment: Alignment.centerLeft,
-          child: builderItem(context, item)),
-    ))
+              value: item,
+              onTap: onTap,
+              child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: builderItem(context, item)),
+            ))
         .toList();
   }
 
@@ -74,8 +79,8 @@ class _Builder extends StatelessWidget {
             selectedItemBuilder: (context) {
               return items
                   .map<Widget>((item) => Container(
-                  alignment: Alignment.centerLeft,
-                  child: builderSelectedItem(context, item)))
+                      alignment: Alignment.centerLeft,
+                      child: builderSelectedItem(context, item)))
                   .toList();
             },
             items: _items(context),
@@ -108,6 +113,9 @@ class DropdownFormField extends FormField<dynamic> {
   /// Triggered when any [DropDownMenuItem] items are selected
   final Function(dynamic)? onChanged;
 
+  /// Triggered when any [DropDownMenuItem] items are selected
+  final Function()? onTap;
+
   /// Called set the style of the form
   final InputDecoration? decoration;
 
@@ -118,32 +126,34 @@ class DropdownFormField extends FormField<dynamic> {
   final Color? iconEnabledColor;
   final Color? dropdownColor;
 
-  DropdownFormField(
-      {required this.items,
-        this.selectedValue,
-        this.decoration,
-        this.iconDisabledColor,
-        this.iconEnabledColor,
-        this.dropdownColor,
-        this.enabled = true,
-        required this.hintBuilder,
-        required this.itemBuilder,
-        required this.selectedItemBuilder,
-        this.onChanged})
-      : super(
-      initialValue: selectedValue,
-      enabled: enabled,
-      builder: (state) => _Builder(
-        state: state,
-        items: items,
-        hintTitle: hintBuilder,
-        decoration: decoration,
-        selectedValue: selectedValue,
-        builderItem: itemBuilder,
-        builderSelectedItem: selectedItemBuilder,
-        onChanged: onChanged,
-        iconDisabledColor: iconDisabledColor,
-        iconEnabledColor: iconEnabledColor,
-        dropdownColor: dropdownColor,
-      ));
+  DropdownFormField({
+    required this.items,
+    this.selectedValue,
+    this.decoration,
+    this.iconDisabledColor,
+    this.iconEnabledColor,
+    this.dropdownColor,
+    this.enabled = true,
+    required this.hintBuilder,
+    required this.itemBuilder,
+    required this.selectedItemBuilder,
+    this.onChanged,
+    this.onTap,
+  }) : super(
+            initialValue: selectedValue,
+            enabled: enabled,
+            builder: (state) => _Builder(
+                  state: state,
+                  items: items,
+                  hintTitle: hintBuilder,
+                  decoration: decoration,
+                  selectedValue: selectedValue,
+                  builderItem: itemBuilder,
+                  builderSelectedItem: selectedItemBuilder,
+                  onChanged: onChanged,
+                  onTap: onTap,
+                  iconDisabledColor: iconDisabledColor,
+                  iconEnabledColor: iconEnabledColor,
+                  dropdownColor: dropdownColor,
+                ));
 }
